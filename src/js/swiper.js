26420232c25c3e5b794b17ff7241b3dd4eba7270
Anka <103377387+ann153476@users.noreply.github.com>
2023-04-22 22:54:25 +0300

    oug

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,1 +1,20 @@
-console.log('///');
+import Swiper, {
+  Navigation,
+  Pagination,
+  EffectCoverflow,
+  Autoplay,
+} from 'swiper';
+
+const swiper = new Swiper('.mySwiper', {
+  modules: [Navigation, Pagination, EffectCoverflow, Autoplay],
+  direction: 'vertical',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
